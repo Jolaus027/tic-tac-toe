6c9101be64427bc4d4088428dee4333098c52dbd
Jolinator <125139458+Jolaus027@users.noreply.github.com>
2023-09-30 22:52:42 +0200

    Update piskorky.py

--- piskorky.py
+++ piskorky.py
@@ -1,45 +1,34 @@
-import tkinter as tk
-from tkinter import messagebox
+import tkinter
 
-# Initialize the main window
-root = tk.Tk()
+canvas = tkinter.Canvas(height= 500, width= 500)
+canvas.pack()
 
-# Initialize variables
-current_player = "X"
-board = [" " for _ in range(9)]
 
-# Create a function to check for a win
-def check_win():
-    for win_combo in [(0, 1, 2), (3, 4, 5), (6, 7, 8),
-                      (0, 3, 6), (1, 4, 7), (2, 5, 8),
-                      (0, 4, 8), (2, 4, 6)]:
-        if board[win_combo[0]] == board[win_combo[1]] == board[win_combo[2]] != " ":
-            return True
-    return False
+x = 20 
+w = 500/x
 
-# Create a function for a button click
-def button_click(index):
-    global current_player
-    if board[index] == " ":
-        board[index] = current_player
-        buttons[index].config(text=current_player)
-        if check_win():
-            messagebox.showinfo("Tic Tac Toe", f"Player {current_player} wins!")
-            root.quit()
-        elif " " not in board:
-            messagebox.showinfo("Tic Tac Toe", "It's a draw!")
-            root.quit()
-        else:
-            current_player = "O" if current_player == "X" else "X"
+for i in range(1,x+1):
 
-# Create buttons
-buttons = [tk.Button(root, text=" ", font=("normal", 20), width=8, height=4,
-                     command=lambda index=i: button_click(index)) for i in range(9)]
+    canvas.create_line(0, 500/x*i,500, 500/x*i)
+    canvas.create_line(500/x*i, 0, 500/x*i, 500)
 
-# Grid layout for buttons
-for i in range(3):
-    for j in range(3):
-        buttons[i * 3 + j].grid(row=i, column=j)
+def klik(a):
 
-# Run the main loop
-root.mainloop()
+    print("x",a.x)
+    print("y",a.y)
+
+    canvas.create_oval(a.x//w*w, a.y//w*w, a.x//w*w+w, a.y//w*w+w, fill="red")
+
+def klik2(c):
+
+
+    x_r = c.x
+    y_r = c.y
+
+    canvas.create_line(x_r // w*w, y_r // w*w, x_r // w*w + 20, y_r // w*w + 20, fill="blue")
+    canvas.create_line(x_r // w*w, y_r // w*w + 20, x_r // w*w + 20, y_r // w*w, fill="blue")
+
+canvas.bind("<Button-1>", klik)
+canvas.bind("<Button-3>", klik2)
+
+tkinter.mainloop()
